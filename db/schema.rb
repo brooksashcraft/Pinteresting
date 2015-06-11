d2001c25a969a72da618b52d937bb7a3d8d9bf2c
brooksashcraft <brxashcraft@gmail.com>
2015-06-10 19:21:30 -0500

    Practiced manually setting up a path

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608235827) do
+ActiveRecord::Schema.define(version: 20150610234624) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "view_count", default: 0
+  end
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
